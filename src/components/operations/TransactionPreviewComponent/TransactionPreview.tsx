eca754e9daadf551f7120940bfd295a8210bf661
Екатерина <k.stepnaya@gmail.com>
2025-06-30 17:38:36 +0300

    feat: small description

--- src/components/operations/TransactionPreviewComponent/TransactionPreview.tsx
+++ src/components/operations/TransactionPreviewComponent/TransactionPreview.tsx
@@ -11,12 +11,14 @@ export interface TransactionPreviewProps {
 
 
 export const TransactionPreview: React.FC<TransactionPreviewProps> = (props) => {
+    const description = props.description;
+    const descSmall = description.length>20?description.substring(0,20)+"...":description;
     return(
         <div className="transaction-preview-div">
             <h2>{props.title}</h2>
             <p>Сумма операции: {props.amount}</p>
             <p>Категория: {props.category}</p>
-            <p>{props.description}</p>
+            <p>{descSmall}</p>
         </div>
     )
 }
